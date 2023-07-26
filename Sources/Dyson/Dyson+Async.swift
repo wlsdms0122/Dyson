8a53c23e3f0ea9654b1e2c957653bd58e5a4ed70
Jeong Jineun <wlsdms0122@gmail.com>
2023-07-26 23:09:39 +0900

    fix(): fix request to cancel when using async-await

--- Sources/Dyson/Dyson+Async.swift
+++ Sources/Dyson/Dyson+Async.swift
@@ -14,20 +14,27 @@ public extension Dyson {
         progress: ((Progress) -> Void)? = nil,
         requestModifier: ((URLRequest) -> URLRequest)? = nil
     ) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            response(
-                spec,
-                progress: progress,
-                requestModifier: requestModifier
-            ) { result in
-                switch result {
-                case let .success(result):
-                    continuation.resume(returning: result)
-                    
-                case let .failure(error):
-                    continuation.resume(throwing: error)
-                }
+        var continuation: CheckedContinuation<(Data, URLResponse), Error>?
+        let sessionTask = response(
+            spec,
+            progress: progress,
+            requestModifier: requestModifier
+        ) { result in
+            switch result {
+            case let .success(result):
+                continuation?.resume(returning: result)
+                
+            case let .failure(error):
+                continuation?.resume(throwing: error)
             }
+        }
+        
+        return try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation {
+                continuation = $0
+            }
+        } onCancel: {
+            sessionTask.cancel()
         }
     }
     
@@ -37,20 +44,27 @@ public extension Dyson {
         progress: ((Progress) -> Void)? = nil,
         requestModifier: ((URLRequest) -> URLRequest)? = nil
     ) async throws -> S.Result {
-        try await withCheckedThrowingContinuation { continuation in
-            data(
-                spec,
-                progress: progress,
-                requestModifier: requestModifier
-            ) { result in
-                switch result {
-                case let .success(result):
-                    continuation.resume(returning: result)
-                    
-                case let .failure(error):
-                    continuation.resume(throwing: error)
-                }
+        var continuation: CheckedContinuation<S.Result, Error>?
+        let sessionTask = data(
+            spec,
+            progress: progress,
+            requestModifier: requestModifier
+        ) { result in
+            switch result {
+            case let .success(result):
+                continuation?.resume(returning: result)
+                
+            case let .failure(error):
+                continuation?.resume(throwing: error)
             }
+        }
+        
+        return try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation {
+                continuation = $0
+            }
+        } onCancel: {
+            sessionTask.cancel()
         }
     }
 }
